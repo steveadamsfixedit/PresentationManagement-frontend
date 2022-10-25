a6b1b372bac1835107e22ecc6db2c68fe09f9681
Raajheer1 <raaj.patel229@gmail.com>
2022-10-25 17:09:31 -0500

    Update rooms page

--- src/views/page/Rooms.vue
+++ src/views/page/Rooms.vue
@@ -2,16 +2,20 @@
   <div class="flex flex-col">
     <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-        <div class="flex justify-between overflow-hidden">
+        <div class="flex  overflow-hidden">
           <div class="flex">
             <label class="ml-4 block relative flex items-center text-zinc-300 focus-within:text-emerald-400">
               <font-awesome-icon :icon="['fas', 'magnifying-glass']" class="w-5 h-5 absolute ml-3 mt-1 pointer-events-none" />
               <input type="search" placeholder="Search..." v-model="search" class="pl-10 pr-3 mt-1 block w-full px-3 py-2 bg-zinc-400 border border-zinc-600 rounded-md text-sm text-zinc-300 shadow-sm placeholder-zinc-600 focus:outline-none focus:border-zinc-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"/>
             </label>
-            <label for="room" class="text-white">Selection which location you want to download files for</label>
-            <select id="room" name="room" v-model="selectedLocation" class="pr-3 mt-1 block w-1/3 px-3 py-2 bg-zinc-400 border border-zinc-600 rounded-md text-sm text-zinc-300 shadow-sm placeholder-zinc-600 focus:outline-none focus:border-zinc-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500">
-              <option v-for="location in locations" :id="location.id" :value="location">{{ location }}</option>
-            </select>
+            <label for="room" class="ml-4 block relative flex items-center text-zinc-300 focus-within:text-emerald-400">
+              <select id="room" name="room" v-model="selectedLocation" class="pr-3 mt-1 block w-full px-3 py-2 bg-zinc-400 border border-zinc-600 rounded-md text-sm text-zinc-300 shadow-sm placeholder-zinc-600 focus:outline-none focus:border-zinc-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500">
+                <option v-for="location in locations" :id="location.id" :value="location">{{ location }}</option>
+              </select>
+            </label>
+          </div>
+          <div class="flex mt-4 mr-4">
+            <download-room-files />
           </div>
         </div>
       </div>
@@ -109,7 +113,8 @@ library.add(faMagnifyingGlass);
 
 // COMPONENTS
 import InfoPresentation from "@/components/modals/InfoPresentation.vue";
-import UploadPresentation from "@/components/modals/UploadPresentation.vue";;
+import UploadPresentation from "@/components/modals/UploadPresentation.vue";
+import DownloadRoomFiles from "@/components/modals/DownloadRoomFiles.vue";
 
 
 // STORES
