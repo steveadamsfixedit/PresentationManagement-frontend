0443d68fd1400120872a13229ac7115cc14f73f6
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2022-11-02 15:34:24 -0500

    Made it wider.

--- src/components/modals/CreatePresentation.vue
+++ src/components/modals/CreatePresentation.vue
@@ -18,7 +18,7 @@
       bg-gray-700 bg-opacity-50
     "
   >
-    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 w-1/3">
+    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 w-1/2">
       <div class="relative p-4 w-full h-full md:h-auto">
         <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" @click="isOpen = false">
           <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -42,21 +42,25 @@
               <label for="speaker" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Speaker Name - Separate multiple by comma (eg. John Smith, Bob Adams)</label>
               <input type="text" v-model="speaker" name="speaker" id="speaker" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="John Adams" required>
             </div>
-            <div>
-              <label for="location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Location</label>
-              <input type="text" v-model="location" name="location" id="location" placeholder="Ballroom 1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+            <div class="grid grid-cols-3 gap-x-3">
+              <div class="col-span-2">
+                <label for="location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Location</label>
+                <input type="text" v-model="location" name="location" id="location" placeholder="Ballroom 1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+              </div>
+              <div>
+                <label for="date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Date(YYYY-MM-DD)</label>
+                <input type="text" v-model="date" name="date" id="date" placeholder="YYYY-MM-DD" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+              </div>
             </div>
-            <div>
-              <label for="date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Date(YYYY-MM-DD)</label>
-              <input type="text" v-model="date" name="date" id="date" placeholder="YYYY-MM-DD" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
-            </div>
-            <div>
-              <label for="time" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Start Time(HH:MM)</label>
-              <input type="text" v-model="time" name="time" id="time" placeholder="HH:MM" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
-            </div>
-            <div>
-              <label for="time" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">End Time(HH:MM)</label>
-              <input type="text" v-model="endtime" name="time" id="time" placeholder="HH:MM" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+            <div class="grid grid-cols-2 gap-x-3">
+              <div>
+                <label for="time" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Start Time(HH:MM)</label>
+                <input type="text" v-model="time" name="time" id="time" placeholder="HH:MM" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+              </div>
+              <div>
+                <label for="time" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">End Time(HH:MM)</label>
+                <input type="text" v-model="endtime" name="time" id="time" placeholder="HH:MM" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+              </div>
             </div>
             <div class="text-center">
               <button type="button" @click="isOpen = false" class="w-1/3 text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-400 dark:hover:bg-gray-500 dark:focus:ring-gray-800 mr-10">Cancel</button>
